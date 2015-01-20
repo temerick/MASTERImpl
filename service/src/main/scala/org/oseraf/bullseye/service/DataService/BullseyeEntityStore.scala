ed85ea2248538f887148fb0bcae57821e790039a
sstyer <sstyer@ccri.com>
2015-01-20 18:22:49 -0500

    removes gremlin-scala from the pom and made the TitanBlueprintsGraphStore inherit from the BlueprintsGraphStore

--- service/src/main/scala/org/oseraf/bullseye/service/DataService/BullseyeEntityStore.scala
+++ service/src/main/scala/org/oseraf/bullseye/service/DataService/BullseyeEntityStore.scala
@@ -57,34 +57,9 @@ class BlueprintsBullseyeEntityStore
   val store: EntityStore = innerStore
 }
 
-class TitanBlueprintsBullseyeEntityStore extends BullseyeEntityStore {
-  var graph: Graph = null
-  var innerStore: TitanBlueprintsGraphStore with WriteEventPublisherPlugin = null
-
+class TitanBlueprintsBullseyeEntityStore extends BlueprintsBullseyeEntityStore {
   override def setup(conf: Config) = {
     graph = GraphLoader.createGraph(conf)
     innerStore = new TitanBlueprintsGraphStore(graph) with WriteEventPublisherPlugin
   }
-
-  override def spliceStore = innerStore
-  override def resolutionStore = innerStore
-  override def mergeIdentifier = new SimpleAddingMergerIdentifier {}
-  override def splitIdentifier = new SimpleAddingSplitterIdentifier {}
-
-  def search(key: AttributeContainer.KEY, value: AttributeContainer.VALUE) = innerStore.search(key, value)
-  def searchableAttributes = innerStore.searchableAttributes
-
-  def addEntity(id: EntityStore.ID, entity: Entity) = innerStore.addEntity(id, entity)
-  def addRelationship(id: EntityStore.ID, relationship: Relationship) = innerStore.addRelationship(id, relationship)
-  def entity(id: EntityStore.ID) = innerStore.entity(id)
-  def entityExists(id: EntityStore.ID) = innerStore.entityExists(id)
-  def relationship(id: EntityStore.ID) = innerStore.relationship(id)
-  def relationshipExists(id: EntityStore.ID) = innerStore.relationshipExists(id)
-  def removeEntity(id: EntityStore.ID) = innerStore.removeEntity(id)
-  def removeRelationship(id: EntityStore.ID) = innerStore.removeRelationship(id)
-  def updateEntity(id: EntityStore.ID, entity: Entity) = innerStore.updateEntity(id, entity)
-  def updateRelationship(id: EntityStore.ID, relationship: Relationship) = innerStore.updateRelationship(id, relationship)
-  def neighborhood(entityId: EntityStore.ID) = innerStore.neighborhood(entityId)
-
-  val store: EntityStore = innerStore
 }
