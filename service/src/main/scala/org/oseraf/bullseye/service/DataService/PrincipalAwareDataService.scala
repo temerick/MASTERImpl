491ad139f6edd40cbb84ee00ca0739271c071268
Nick Hamblet <nhamblet@ccri.com>
2015-02-28 09:22:40 -0500

    Added null checks to PrincipalAwareDataService
    If users are not required to log in, they should still be allowed to merge and split
    entities.

--- service/src/main/scala/org/oseraf/bullseye/service/DataService/PrincipalAwareDataService.scala
+++ service/src/main/scala/org/oseraf/bullseye/service/DataService/PrincipalAwareDataService.scala
@@ -52,19 +52,23 @@ trait PrincipalAwareDataService extends DataService {
   }
   private def isOK(p:Principal, entity:Entity):Boolean = {
     entity.attributes.get(AUTH_KEY) match {
-      case Some(pName) => pName == p.getName
+      case Some(pName) => p != null && pName == p.getName
       case _ => true //there is no Authorization key/value so this entity is globally available
     }
   }
   def merge(p:Principal, entityIds: Seq[EntityStore.ID], mergedEntity:BullsEyeEntity): BullsEyeEntity = {
     val visibleEdgeFilteredEntities = entityIds.flatMap(eId => getBullsEyeEntityDetails(p, eId))
-    val newMergedEntity = mergedEntity.copy(attrs=mergedEntity.attrs + (AUTH_KEY -> p.getName))
+    val newMergedEntity =
+      if (p == null) mergedEntity
+      else mergedEntity.copy(attrs=mergedEntity.attrs + (AUTH_KEY -> p.getName))
     super.merge(visibleEdgeFilteredEntities.map(_.id), newMergedEntity)
   }
   def split(p:Principal, entityId: EntityStore.ID, splitEntities:Seq[BullsEyeEntity]): Seq[BullsEyeEntity] = {
     getBullsEyeEntityDetails(p, entityId) match {
       case Some(ent) => {
-        val newSplitEntities = splitEntities.map(e => e.copy(attrs=e.attrs + (AUTH_KEY -> p.getName)))
+        val newSplitEntities =
+          if (p == null) splitEntities
+          else splitEntities.map(e => e.copy(attrs=e.attrs + (AUTH_KEY -> p.getName)))
         super.split(entityId, newSplitEntities)
       }
       case _ => List()
