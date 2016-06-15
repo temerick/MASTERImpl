3151b458c0976724294870da728f8aa6e0d147d7
Kevin Hobbs <khobbs@ccri>
2016-06-15 14:03:52 -0400

    add CORS support again

--- webapp/src/main/scala/org/oseraf/bullseye/webapp/api/API.scala
+++ webapp/src/main/scala/org/oseraf/bullseye/webapp/api/API.scala
@@ -1,15 +1,18 @@
 package org.oseraf.bullseye.webapp.api
 
 import org.json4s.{DefaultFormats, Formats}
-import org.scalatra.ScalatraServlet
+import org.scalatra.{ScalatraServlet,CorsSupport}
 import org.scalatra.json.JacksonJsonSupport
 
 trait APIMarshalling extends JacksonJsonSupport {
   protected implicit val jsonFormats: Formats = DefaultFormats
 }
 
-trait API extends ScalatraServlet with APIMarshalling {
+trait API extends ScalatraServlet with APIMarshalling with CorsSupport {
   before() {
     contentType = formats("json")
+    options("/*"){
+      response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"))
+    }
   }
 }
